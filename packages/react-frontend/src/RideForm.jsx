36605beace1080eb8001132c1ef91f06570998f3
Jacroney <Jacroney24@gmail.com>
2026-02-19 15:13:27 -0800

    Remove react template. Replace with landing page grey background. Add cal poly green navbar at top along with PolyPool.

--- packages/react-frontend/src/RideForm.jsx
+++ packages/react-frontend/src/RideForm.jsx
@@ -1,131 +1,127 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 function RideForm(props) {
-    const [ride, setRide] = useState({
-        starting_point: "Cal Poly",
-        destination: "",
-        start_time: "",
-        driver: "",
-        other_riders: [],
-        cost: 0,
-        car: "",
-        seats: 0,
-        deviation: 0,
+  const [ride, setRide] = useState({
+    starting_point: 'Cal Poly',
+    destination: '',
+    start_time: '',
+    driver: '',
+    other_riders: [],
+    cost: 0,
+    car: '',
+    seats: 0,
+    deviation: 0,
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setRide({
+      ...ride,
+      [name]: value,
     });
+  }
 
+  function submitForm() {
+    props.handleSubmit(ride);
+    setRide({
+      starting_point: 'Cal Poly',
+      destination: '',
+      start_time: '',
+      driver: '',
+      other_riders: [],
+      cost: 0,
+      car: '',
+      seats: 0,
+      deviation: 0,
+    });
+  }
 
-    function handleChange(event) {
-        const { name, value } = event.target;
-        setRide({
-        ...ride,
-        [name]: value,
-        });
-    }
-
-    function submitForm() {
-        props.handleSubmit(ride);
-        setRide({ 
-        starting_point: "Cal Poly",
-        destination: "",
-        start_time: "",
-        driver: "",
-        other_riders: [],
-        cost: 0,
-        car: "",
-        seats: 0,
-        deviation: 0, 
-        });
-    }
-
-
-    return (
+  return (
     <form>
-        <label htmlFor="starting_point">Starting Point</label>
-        <input
+      <label htmlFor="starting_point">Starting Point</label>
+      <input
         type="text"
         name="starting_point"
         id="starting_point"
         value={ride.starting_point}
         onChange={handleChange}
-        />
+      />
 
-        <label htmlFor="destination">Destination</label>
-        <input
+      <label htmlFor="destination">Destination</label>
+      <input
         type="text"
         name="destination"
         id="destination"
         value={ride.destination}
         onChange={handleChange}
-        />
+      />
 
-        <label htmlFor="start_time">Start Time</label>
-        <input
+      <label htmlFor="start_time">Start Time</label>
+      <input
         type="datetime-local"
         name="start_time"
         id="start_time"
         value={ride.start_time}
         onChange={handleChange}
-        />
+      />
 
-        <label htmlFor="driver">Driver</label>
-        <input
+      <label htmlFor="driver">Driver</label>
+      <input
         type="text"
         name="driver"
         id="driver"
         value={ride.driver}
         onChange={handleChange}
-        />
+      />
 
-        <label htmlFor="other_riders">Other Riders</label>
-        <input
+      <label htmlFor="other_riders">Other Riders</label>
+      <input
         type="text"
         name="other_riders"
         id="other_riders"
         value={ride.other_riders}
         onChange={handleChange}
-        />
+      />
 
-        <label htmlFor="cost">Cost</label>
-        <input
+      <label htmlFor="cost">Cost</label>
+      <input
         type="number"
         name="cost"
         id="cost"
         value={ride.cost}
         onChange={handleChange}
-        />
+      />
 
-        <label htmlFor="car">Car</label>
-        <input
+      <label htmlFor="car">Car</label>
+      <input
         type="text"
         name="car"
         id="car"
         value={ride.car}
         onChange={handleChange}
-        />
+      />
 
-        <label htmlFor="seats">Seats</label>
-        <input
+      <label htmlFor="seats">Seats</label>
+      <input
         type="number"
         name="seats"
         id="seats"
         value={ride.seats}
         onChange={handleChange}
-        />
+      />
 
-        <label htmlFor="deviation">Deviation</label>
-        <input
+      <label htmlFor="deviation">Deviation</label>
+      <input
         type="number"
         name="deviation"
         id="deviation"
         value={ride.deviation}
         onChange={handleChange}
-        />
+      />
 
-
-        <input type="button" value="Submit" onClick={submitForm} />
+      <input type="button" value="Submit" onClick={submitForm} />
     </form>
-    );
-
+  );
 }
 
 export default RideForm;
