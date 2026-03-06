6108b29fb734e7f105f8a68aeef5f50144c982ac
Akshat Khandelwal <akshatk1001@gmail.com>
2026-03-06 09:57:14 -0800

    testing CI

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -251,3 +251,45 @@ app.get('/api/cities', async (req, res) => {
   }
 });
 
+
+//Google maps Api requests
+app.get('/api/maps/route', async (req, res ) =>{
+  const origin = req.query.origin;
+  const dest = req.query.dest;
+
+  if(!origin || !dest){
+    res.status(400).json({message: "Bad Request: origin or destination not provided"});
+    return;
+  }
+
+  let ComputeRoutesRequest = {
+    "origin": {
+      "location": {
+        "address": `${origin}, CA`
+      },
+    },
+    "destination": {
+      "location": {
+        "address": `${dest}, CA`
+      },
+    },
+    "routing_preference": "TRAFFIC_AWARE",
+    "travel_mode": "DRIVE",
+    "polylineQuality": "HIGH_QUALITY"
+  };
+
+  try {
+    const route = fetch('https://routes.googleapis.com/directions/v2:computeRoutes', {
+      method : 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Goog-Api-Key': 'AIzaSyA1xtEjGzdHRTBSfxC6MgHgIW-oPT7tQ2c',
+        'X-Goog-FieldMask': 'routes.duration,routes.'
+
+      }
+    })
+    res.status(200).json(route);
+  }catch (error){
+    res.status(500).json({error: error.message})
+  }
+});
