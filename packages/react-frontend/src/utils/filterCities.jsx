4ce8fb5bc65ffa94cd0498a7cde2ce5cada24b7b
Akshat Khandelwal <akshatk1001@gmail.com>
2026-03-14 18:10:25 -0700

    ran formatter

--- packages/react-frontend/src/utils/filterCities.jsx
+++ packages/react-frontend/src/utils/filterCities.jsx
@@ -2,10 +2,7 @@ const filterCities = (cities, query) => {
   const normalizedQuery = query.trim().toLowerCase();
 
   return cities
-    .filter(
-      (city) =>
-        city.name.toLowerCase().includes(normalizedQuery),
-    )
+    .filter((city) => city.name.toLowerCase().includes(normalizedQuery))
     .slice(0, 10);
 };
 
