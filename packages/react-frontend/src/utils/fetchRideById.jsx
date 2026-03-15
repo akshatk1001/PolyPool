bcc9ba64feac134ba1581b0497cc11977993a9b0
Akshat Khandelwal <akshatk1001@gmail.com>
2026-03-15 00:29:06 -0700

    added ability to fetch ride by id

---
+++ packages/react-frontend/src/utils/fetchRideById.jsx
@@ -0,0 +1,11 @@
+import { API_URL } from '../constants/api';
+
+async function fetchRideById(id) {
+    const response = await fetch(`${API_URL}/api/rides/${id}`, {
+      credentials: 'include',
+    });
+    if (!response.ok) return null;
+    return response.json();
+  }
+
+export default fetchRideById;
